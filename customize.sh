b53e0cec0a819c8dc8a25833230ff1c2b1cfd45c
Rve27 <109242186+Rve27@users.noreply.github.com>
2023-10-15 20:46:52 +0700

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,24 @@
+. ${SH:=$MODPATH/ohmyfont}
+
+### INSTALLATION ###
+
+ui_print '- Installing'
+
+ui_print '+ 20%'
+prep
+
+ui_print '+ 40%'
+config
+
+ui_print '+ 60%'
+install_font
+
+src
+
+ui_print '+ 80%'
+rom
+
+ui_print '- 100%'
+fontspoof
+svc
+finish
